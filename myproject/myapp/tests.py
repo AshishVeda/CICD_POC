72a1639644694a0b54fe2e64d8f0cca615de39dc
AshishVeda <vedaashish2@gmail.com>
2024-07-19 00:46:58 -0700

    Test

--- myproject/myapp/tests.py
+++ myproject/myapp/tests.py
@@ -4,4 +4,5 @@ from .models import MyModel  # Replace with your actual model
 class MyModelTest(TestCase):
     def test_str(self):
         my_model = MyModel(name="Test")
+        
         self.assertEqual(str(my_model), "Test")
